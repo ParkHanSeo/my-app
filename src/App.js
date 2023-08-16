8895ab22bbcbd117c8f156eaea7e10e9846f5d32
[NDCC] Park jee-hoon <jh.m02h.park@misumi.co.kr>
2023-08-16 14:19:21 +0900

    2023.08.16 리액트 환경설정, 소스코드 수정, 컴포넌트 생성, props 활용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props){
+  console.log('props', props, props.title);
+  return <header>
+    <h1><a href="/">{props.title}</a></h1>
+  </header>
+}
+function Nav(props){
+  const lis = []
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
+function Article(props){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
 function App() {
+  const header_title = 'WEB'
+
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'}
+  ]
+
+  const article_data = {
+    title:'welcome', body:'Hi, WEB'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title={header_title}></Header>
+      <Nav topics={topics}></Nav>
+      <Article title={article_data.title} body={article_data.body}></Article>
     </div>
   );
 }
